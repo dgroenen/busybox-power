41a43ead399096d0858378b4afa7fbfe82d0af92
Dennis Groenen <tj.groenen@gmail.com>
2012-04-22 21:38:44 +0200

    update busybox-power against BusyBox 1.20 release

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Helper script to build busybox-power
 # Please run me from within Scratchbox
 
-BBVERSION="1.19.4"
+BBVERSION="1.20.0"
 MAKETHREADS="8"
 SCRIPTDIR=`dirname $(readlink -f $0)`
 BUILDDIR="$SCRIPTDIR/../busybox-power-build"
