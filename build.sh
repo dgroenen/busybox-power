1734dbb6222f7af79b0006b14648a292be9f5fa2
Dennis Groenen <tj.groenen@gmail.com>
2012-06-30 15:57:32 +0200

    revert thumb-related changes in master

--- build.sh
+++ build.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
 # Helper script to build busybox-power
 # Please run me from within Scratchbox
-#
-# Run `sh build.sh thumb` to enable the thumb ISA. Make sure your Scratchbox
-# target is properly set up to compile thumb binaries.
-# Instructions to set up a Fremantle thumb target in Scratchbox:
-# http://talk.maemo.org/showpost.php?p=1223814&postcount=164
 
 BBVERSION="1.20.1"
 MAKETHREADS=`grep -i 'processor.:' /proc/cpuinfo |wc -l`
 if [ -z "$MAKETHREADS" -o "$MAKETHREADS" -eq 0 ] ; then MAKETHREADS=1; fi
 SCRIPTDIR=`dirname $(readlink -f $0)`
 BUILDDIR="$SCRIPTDIR/../busybox-power-build"
-VERSION_DEBIAN=`cat $SCRIPTDIR/debian/changelog | awk -F'[()]' '{if(NR==1) print $2}'`
 
 BUILD_OPTIONS="parallel=$MAKETHREADS"
-THUMB=false
-
-if [ "$1" == "thumb" ]; then
-  THUMB=true; fi
 
 hash wget 2>&- || { 
   echo >&2 "this script requires wget, exiting now"
@@ -39,13 +29,6 @@ fi
 
 tar -jxf busybox-$BBVERSION.tar.bz2
 cp -af $SCRIPTDIR/debian/ busybox-$BBVERSION/
-
-if $THUMB; then
-  # Append "~thumb" to the package's Debian version string
-  # NB: we feed sed directly with $VERSION_DEBIAN, it is not escaped in any way
-  sed -i "s/$VERSION_DEBIAN/$VERSION_DEBIAN~thumb/" busybox-$BBVERSION/debian/changelog
-  BUILD_OPTIONS="$BUILD_OPTIONS,thumb,vfp"
-fi
 
 # Build
 cd busybox-$BBVERSION/ && DEB_BUILD_OPTIONS="$BUILD_OPTIONS" dpkg-buildpackage -r"fakeroot -u" -uc -us -nc
